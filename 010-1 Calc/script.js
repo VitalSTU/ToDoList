509eba9eb4e2bb14f00fe7ff2751fe7dc34721c9
VitalSTU <kolysvit@mail.ru>
2022-01-24 23:16:28 +0300

    Calc done

--- 010-1 Calc/script.js
+++ 010-1 Calc/script.js
@@ -1,21 +1,35 @@
-function calc(action, var1, var2) {
+function Calc(action, var1, var2) {
     if (action === undefined || var1 === undefined || var2 === undefined) {
         alert('Error! calc must has 3 parameters');
     } else {
-        !isNumber(var1) ? alert(var1 + "Error! var1 isn't number") : 
-                !isNumber(var2) ? alert(var2 + "Error! var2 isn't number") : doAction(action, var1, var2);
+        !isNumber(+var1) ? alert(var1 + "Error! var1 isn't number") : 
+                !isNumber(+var2) ? alert(var2 + "Error! var2 isn't number") : doAction(action, +var1, +var2);
     }
 }
 
 function isNumber(x) {
-    //return x.length > 0 && !isNaN(+x);
     return typeof(x) === 'number';
 }
 
 function doAction(action, var1, var2) {
     switch (action) {
         case 'sum':
-            alert(+var1 + +var2);
+            alert(var1 + var2);
+            break;
+        case 'sub':
+            alert(var1 - var2);
+            break;
+        case 'mul':
+            alert(var1 * var2);
+            break;
+        case 'div':
+            alert(var1 / var2);
+            break;
+        case 'rem':
+            alert(var1 % var2);
+            break;
+        case 'pow':
+            alert(var1 ** var2);
             break;
         default:
             alert("Command hasn't recognised.");
@@ -23,4 +37,11 @@ function doAction(action, var1, var2) {
     };
 }
 
-calc(prompt('Введите команду'), prompt('Введите число'), prompt('Введите число'));
+console.log(Calc('sum', 2, 5));
+console.log(Calc('sub', 2, 5));
+console.log(Calc('mul', 2, 5));
+console.log(Calc('div', 2, 5));
+console.log(Calc('rem', 2, 5));
+console.log(Calc('pow', 2, 5));
+
+Calc(prompt('Введите команду'), prompt('Введите число'), prompt('Введите число'));
