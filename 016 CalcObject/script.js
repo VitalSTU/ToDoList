5eb59181e51eaa58e42dc089c911da64b7d4b1bf
VitalSTU <kolysvit@mail.ru>
2022-01-27 00:07:17 +0300

    Lesson 016 home work: simple calculator through plain object

--- 016 CalcObject/script.js
+++ 016 CalcObject/script.js
@@ -1,38 +1,29 @@
+//TODO Calc through object
 function Calc(action, var1, var2) {
     let paramsQtyError = action === undefined || var1 === undefined || var2 === undefined;
+
+    let calculator = {
+        sum: var1 + var2,
+        sub: var1 - var2,
+        mult: var1 * var2,
+        div: var1 / var2,
+    }
     
     if (paramsQtyError) {
         return 'Error! There are must be 3 parameters.';
+    } else if (!(action in calculator)) {
+        return "Error! Unknown operation.";
     } else if (!isNumber(var1)) {
         return "Error! Variable 1 isn't number.";
     } else if (!isNumber(var2)) {
         return "Error! Variable 2 isn't number";
     }
-    return calculate(action, +var1, +var2);
+
+    return calculator[action];
 }
 
-function isNumber(x) {
-    return !isNaN(x) && typeof(x) === 'number';
-}
-
-function calculate(action, var1, var2) {
-    switch (action) {
-        case 'sum':
-            return var1 + var2;
-            break;
-        case 'sub':
-            return var1 - var2;
-            break;
-        case 'mult':
-            return var1 * var2;
-            break;
-        case 'div':
-            return var1 / var2;
-            break;
-        default:
-            return "Error! Unknown operation.";
-            break;
-    };
+function isNumber(n) {
+    return !isNaN(n) && typeof(n) === 'number';
 }
 
 console.log(Calc('sum', 2, 5));
