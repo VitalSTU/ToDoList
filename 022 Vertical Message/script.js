33eb5a21c689357069488cc3383b5d3d9c37597a
VitalSTU <kolysvit@mail.ru>
2022-01-31 20:38:27 +0300

    022 Vertical Message

---
+++ 022 Vertical Message/script.js
@@ -0,0 +1,18 @@
+
+function showVerticalMessage(str) {
+    let m = 'м';
+    let result = str.startsWith(m) ? m.toUpperCase() : str[0];
+    //result += '\n';
+    for (let char of (str.slice(1, 10))) {
+        result += `\n${char}`;
+    }
+
+    return result;
+}
+
+//Function test
+console.log(showVerticalMessage('ABC'));
+console.log();
+console.log(showVerticalMessage('ABCDEFGHIJabcdefghij'));
+console.log();
+console.log(showVerticalMessage('марафон'));
