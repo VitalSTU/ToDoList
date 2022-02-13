6ff640053bdc2f28046aa51a0284dba1ed0dfff2
VitalSTU <kolysvit@mail.ru>
2022-02-13 16:51:23 +0300

    #031 Delayed Execution done

--- 031 Delayed Execution/script.js
+++ 031 Delayed Execution/script.js
@@ -1,16 +1,28 @@
 // 30 - Delayed Execution
 
-//Вывод каждую секунду
-function printNumbers(from, to) {
-    let i = from;
-    let timerId = setInterval(function() {
-        console.log(i);
-        if (++i > to) clearTimeout(timerId);
-    }, 1000);
-}
+
+// 1 - Вывод каждую секунду
+
 //setInterval
-printNumbers(5, 8);
+function printNumbersInterval(from, to) {
+    let n = from;
+    let timerId = setInterval(() => {
+        console.log(n);
+        if (n++ == to) {
+            clearInterval(timerId);
+        }
+    }, 1000, n);
+}
+printNumbersInterval(3, 8);
+
 //setTimeout
-
-
-//Что покажет setTimeout?
+function printNumbersTimeout(from, to) {
+    let n = from;
+    setTimeout(function repeat() {
+        console.log(n);
+        if (n++ < to) {
+            setTimeout(repeat, 1000);
+        }
+    }, 1000, n);
+}
+printNumbersTimeout(3, 8);
